2fc5a5b67b46109613a2cf1826b850c721d845e8
enderyasli <enderyasli@gmail.com>
2025-03-30 07:32:51 +0300

    second commit

--- app/src/main/java/com/enderyasli/retrofitcoroutines/service/SimpleAPI.kt
+++ app/src/main/java/com/enderyasli/retrofitcoroutines/service/SimpleAPI.kt
@@ -2,7 +2,6 @@ package com.enderyasli.retrofitcoroutines.service
 
 
 import com.enderyasli.retrofitcoroutines.data.User
-import com.enderyasli.retrofitcoroutines.utils.Resource
 import retrofit2.Response
 import retrofit2.http.GET
 
